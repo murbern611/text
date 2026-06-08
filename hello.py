82ad5e2515f8ce1e6066d62f0e53876eabad43fa
murbern <gejx0611@qq.com>
2026-06-08 15:53:34 +0800

    第一次提交：初始化仓库

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""我的第一个 Python 脚本"""
+
+
+def main():
+    print("Hello, GitHub! 👋")
+    print("这是我从本地推送的第一个仓库！")
+
+
+if __name__ == "__main__":
+    main()
